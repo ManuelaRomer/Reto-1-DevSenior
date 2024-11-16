c1a3cfaace51dbd0fbb87bcf728e152972a604de
ManuelaRomer <167833875+ManuelaRomer@users.noreply.github.com>
2024-11-16 16:37:07 -0500

    Update reto-1.py

---
+++ reto-1.py
@@ -0,0 +1,54 @@
+#Maquetacion y estructuración delproyecto de reto 1 
+# 1 RECOPILACIÓN DE DATOS 
+# 2 ANALISIS DE RESULTADOS EXPERIMENTALES 
+# 3 GENERACIÓN DE UN INFORME 
+
+
+import datetime
+#PLATEMAOS LAS FUNCIONES 
+listaDeExperimentos = [
+    ["Experimento 1", "16/11/2024", "Quimica", [5,3,4,5,6,4]]
+]
+
+# "Atributos: nombre, fecha "DD/MM/AAA", tipo y resultsdos numericos."
+
+def agregarExperimentos():
+    pass
+
+def eliminarExperimento(): #dificultad 1 uso de agregar experimento 
+    pass
+
+def visualizarExperimentos(): # dificultad 1 requiere uso funcion de agregar experimento
+    pass 
+
+def calcularEstadisticas(): # dificultad 2 requiere uso de funcion agregarexperimento
+    pass #(promedio, maximos y minimos de un experimento)
+
+def comprarExperimentos(): # dificultad 2 
+    pass #Compara 2 o mas experimentos para determinar los mejores y peores resultados
+
+def generarInforme(): # dificultad: 3 requiere que calcularestadisticas uso de funciones
+    pass
+
+#MENÚ DE OPCIONES CON: 
+
+def mostrarMenu():
+    #dificultad: 1 
+    print("=====MENÚ====")
+    print("1. Agregar experimento")
+    print("2. Visualizar el experimento")
+    print("3. Eliminar experimentos")
+    print("====ANÁLISIS DE DATOS====")
+    print("4. Calcular estadisticas")
+    print("5. Comprar experimentos")
+    print("=====INFORMES====")
+    print("6. Generar informe")
+    print("=====SALIR====")
+
+def main():# controla el flujo dificulta 1 
+    mostrarMenu()
+    pass
+
+#llamamos la función 
+main()
+
