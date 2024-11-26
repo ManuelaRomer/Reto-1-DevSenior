04f57eb0ac5bd01a75c8da0831c15c267a5eef48
ManuelaRomer <167833875+ManuelaRomer@users.noreply.github.com>
2024-11-26 03:33:35 -0500

    feat

--- main.py
+++ main.py
@@ -31,10 +31,11 @@ def agregarExperimento(listaExperimentos):
         print("Resultados no válidos.")
         return
     
+# se crea el objeto y se agrega a lista de experimentos
     experimento = Experimento(nombre, fechaExp, tipo, resultados)
     listaExperimentos.append(experimento)
     print("Su Experimento se ha guardado.")
-    
+
 # con esta funcionpodemos visaulizar el experimento con todos sus atributos
 def visualizarExperimento(listaExperimentos):
     if not listaExperimentos:
@@ -46,6 +47,28 @@ def visualizarExperimento(listaExperimentos):
         print(f"Fecha: {experimento.fechaExp.strftime('%d/%m/%Y')}")
         print(f"Tipo: {experimento.tipo}")
         print(f"Resultados: {experimento.resultados}")
+
+# con esta funcion podremos eliminar un experimento
+def eliminarExperimento(listaExperimentos):
+    if not listaExperimentos:
+        print("No hay experimentos registrados, no se puede eliminar nada")
+        return
+    
+    nombre = input("Ingrese el nombre del experimento a eliminar:")
+# buscamos el experimento por el nombre para saber cual eliminar
+    for i, experimento in enumerate(listaExperimentos):
+        if experimento.nombre == nombre: 
+            comprobar = input(f"¿Desea eliminar el experimento '{nombre}'? R: si o no: ").strip().lower()
+            if comprobar == "si":
+                listaExperimentos.pop(i)
+                print(f"El experimento '{nombre}' se elimino correctamente")
+                return 
+            else:
+                print("El experimento no fue eliminado")
+                return
+    print(f"El experimento '{nombre}' no fue encontrado")
+    
+
 
 def analizarExperimentos(listaExperimentos):
     if not listaExperimentos:
@@ -60,4 +83,98 @@ def analizarExperimentos(listaExperimentos):
         print(f"Máximo de  resultados: {maximo}")
         print(f"Mínimo de resultados: {minimo}")
 
+# función para comparar los experimentos 
+def compararExperimentos(listaExperimentos):
+    if len(listaExperimentos) < 2: 
+        print("Deben de haber como minimo 2 experimentos")
+        return
+    print("Experimentos registrados:")
+    for i, Experimento in enumerate(listaExperimentos):
+        print(f"{i+1}. {Experimento.nombre}")
+        
+    try:
+        # sellecionamos los experimentos a comparar
+        primer1 = int(input("Seleccione el número del primer experimento a comparar: ")) - 1
+        segundo2 = int(input("Seleccione el número del segundo experimento a comparar: ")) - 1
+        
+        if primer1 < 0 or primer1 >= len(listaExperimentos) or segundo2 < 0 or segundo2 >= len(listaExperimentos):
+            print("Numero no valido porfavor ingresa uno correcto")
+            return
+
+        experimento1 = listaExperimentos[primer1]
+        experimento2 = listaExperimentos[segundo2]
+        
+        # calculamos promedios
+        promedio1 = statistics.mean(experimento1.resultados)
+        promedio2 = statistics.mean(experimento2.resultados)
+        
+        # Mostrar resultados de comparación
+        print(f"\nComparación entre {experimento1.nombre} y {experimento2.nombre}:")
+        print(f"{experimento1.nombre} - Promedio: {promedio1}")
+        print(f"{experimento2.nombre} - Promedio: {promedio2}")
+        
+        if promedio1 > promedio2:
+            print(f"El experimento '{experimento1.nombre}' tiene un promedio mayor.")
+        elif promedio1 < promedio2:
+            print(f"El experimento '{experimento2.nombre}' tiene un promedio mayor.")
+        else:
+            print(f"los dos experimentos tienen el mismo promedio.")
+    
+    except ValueError:
+        print("Respuesta no valida")
+        
+    
+
+# funcion para generar un informe de todos los experimentos 
+def generarInforme(listaExperimentos):
+    if not listaExperimentos:
+        print("No hay experimentos registrados")
+        return
+    
+    # abrimos un archivo de texto para realizar el informe 
+    with open("informe_experimento.txt", "w") as archivo:
+        #escribimos los detalles que queremos que contenga
+        for experimento in listaExperimentos:
+            archivo.write(f"Nombre: {experimento.nombre}\n")
+            archivo.write(f"Fecha experimento: {experimento.fechaExp.strftime("%d/%m/%Y")}\n")
+            archivo.write(f"Tipo: {experimento.tipo}\n")
+            archivo.write(f"Resultados: {experimento.resultados}\n")
+            archivo.write("\n")
+    print("Informe generado como 'informe_experimento.txt'")
+
+# funcion para el menu de interaccion
+def menu():
+    listaExperimentos = []
+    while True:
+        print("\nMENU DE OPCIONES:")
+        print("1. Agregar Experimentos")
+        print("2. Visualizar Experimentos")
+        print("3. Eliminar experimento")
+        print("4. Analizar Experimento")
+        print("5. Comparar Experimentos")
+        print("6. Generar Informe")
+        print("7. Salir")
+        
+        opcion = input("Seleccione la opción: ")
+        
+        if opcion == "1":
+            agregarExperimento(listaExperimentos)
+        elif opcion == "2":
+            visualizarExperimento(listaExperimentos)
+        elif opcion == "3":
+            eliminarExperimento(listaExperimentos)
+        elif opcion == "4":
+            analizarExperimentos(listaExperimentos)
+        elif opcion == "5":
+            compararExperimentos(listaExperimentos)
+        elif opcion == "6":
+            generarInforme(listaExperimentos)
+        elif opcion == "7":
+            print("Salirendo")
+            break
+        else:
+            print("Opción no válida.")
+
+if __name__ == "__main__":
+    menu()
 
